bd5fe2ae3f88cafafe5c42888292907806312748
ebramfadl <ebram@ntsal.com>
2023-08-21 17:11:02 +0300

    Tasks list done

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -2,3 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:todo/widgets/task_tile.dart';
 import 'package:provider/provider.dart';
 import 'package:todo/model/task_data.dart';
+
+class TasksList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TaskData>(builder: (context, taskData, child) {
+      return ListView.builder(
+        itemBuilder: (context, index) {
+          final task = taskData.tasks[index];
+          return TaskTile(
+            isChecked: task.isDone,
+            taskTitle: task.name,
+            checkBoxCallback: (checkBoxState) {
+              taskData.updateTask(task);
+            },
+            longPressCallback: () {
+              taskData.deleteTask(task);
+            },
+          );
+        },
+        itemCount: taskData.taskCount,
+      );
+    });
+  }
+}
